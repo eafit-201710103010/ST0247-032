2cbb9211b420550a52cc9f5b01f52e52d75bad4f
Luisa Maria Vasquez Gomez <30305230+eafit-201710103010@users.noreply.github.com>
2018-02-08 22:12:10 -0500

    Update Taller2.java

--- talleres/taller02/Taller2.java
+++ talleres/taller02/Taller2.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 /**
  * Clase en la cual se implementan los metodos del Taller de Clase #2
+ * Realizado por: Juan Jose Parra y Luisa Maria Vasquez
  * 
  * @author Mauricio Toro, Mateo Agudelo
  */
@@ -47,7 +48,7 @@ public class Taller2 {
         System.out.print(" ");
         for (int i = 0; i < n; ++i)
             System.out.print(i + " ");
-        System.out.println("\n");
+            System.out.println();
         for (int i = 0; i < n; ++i) {
             System.out.print(i + " ");
             for (int j = 0; j < n; ++j)
@@ -57,16 +58,41 @@ public class Taller2 {
         System.out.println();
     }
 
-    public static boolean esValido(int[] tablero) {
-        if(false){
-            return false;
+    public static boolean esValido(int[] queens) {
+        for(int i = 0; i < queens.length; i++){
+            for(int j = i+1; j < queens.length; j++){
+                if(Math.abs(queens[i] - queens[j]) == Math.abs(i - j)){
+                    return false;
+                }
+            }
         }
-        imprimirTablero(tablero);
+        imprimirTablero(queens);
         return true;
     }
 
-    public static int queens(int n, int x) {
-
+    private static int queens(String pos, String pre, int n, int[] queens, int[] cuenta) {
+        if(pre.length() == 0){
+            for(int i = 0; i < n; i++){
+                queens[i] = Integer.parseInt(pos.charAt(i)+"");
+            }
+            if(esValido(queens)){
+                cuenta[0]++;
+            }
+        } else {
+            for (int i = 0; i < pre.length(); i++){
+                queens(pos + pre.charAt(i), pre.substring(0,i) + pre.substring(i+1, pre.length()), n, queens, cuenta);
+            }
+        }
+        return cuenta[0];
+    }
+    public static int queens(int n) {
+        int[] cuenta = {0};
+        int[] queens = new int[n];
+        String s = "";
+        for(int i = 0; i < n; i++){
+            s += i;
+        }
+        return queens("",s,n,queens, cuenta);
     }
 }
 
